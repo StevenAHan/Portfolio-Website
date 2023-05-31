9850ae8c6863bb1f97152a4e1fe917ce5f64a1fc
majikyouser <sahiagb@gmail.com>
2023-05-30 20:13:44 -0400

    yes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,8 +6,8 @@ const Header = () => {
         <>
             {/* <img className="OpenerBackground" src={titleImg} alt="hi" /> */}
             <div className="OpenerContainer">
-                <h1 className="Opener">Hello!</h1>
-                <h2 className="OpenerCaps">I am Steven Han, a student at NYU studying for a BS in Computer Science.</h2>
+                <h1 className="Opener title">Hello!</h1>
+                <h2 className="OpenerCaps title">I am Steven Han, a student at NYU studying for a BS in Computer Science.</h2>
             </div>
         </>
     );
