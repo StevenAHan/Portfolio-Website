d8f0e3fa2bd94f4951d83457a1514541414768a7
majikyouser <sahiagb@gmail.com>
2023-06-01 22:52:32 -0400

    font changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,7 +7,7 @@ const Header = () => {
             {/* <img className="OpenerBackground" src={titleImg} alt="hi" /> */}
             <div className="OpenerContainer">
                 <h1 className="Opener title">Hello! I am Steven Han</h1>
-                <h2 className="OpenerCaps title">A student at NYU studying for a BS in Computer Science. I am currently a software engineer intern at Bank of America.</h2>
+                <h2 className="OpenerCaps">A student at NYU studying for a BS in Computer Science. I am currently a software engineer intern at Bank of America.</h2>
                 <br />
                 <div className="center-div">
                     <a href="https://github.com/StevenAHan" className="btn-blue-outline mx-1 white-text" target="_blank" rel="noreferrer"><i className="fab fa-github"></i> Github</a>
