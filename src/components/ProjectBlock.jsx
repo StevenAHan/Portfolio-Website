c7fddefddd361bf4e649dc833c5527d300434882
majikyouser <sahiagb@gmail.com>
2023-06-29 21:07:35 -0400

    Added project skills

--- src/components/ProjectBlock.jsx
+++ src/components/ProjectBlock.jsx
@@ -1,14 +1,63 @@
 import "./css/projectBlock.css";
 import link from "./imgs/link.png";
 import ghlogo from "./imgs/githublogo.png";
+import javascriptImg from "./svg/js.svg";
+import cplusImg from "./svg/c++.svg";
+import pythonImg from "./svg/python.svg";
+import javaImg from "./svg/java.svg";
+import htmlImg from "./svg/html.svg";
+import cssImg from "./svg/css.svg";
+import csharpImg from "./svg/c.svg";
+import cImg from "./svg/csharp.svg";
+import mongodbImg from "./svg/mongodb.svg";
+import mysqlImg from "./svg/mysql.svg";
+import nodejsImg from "./svg/nodejs.svg";
+import reactImg from "./svg/react.svg";
+import flaskImg from "./svg/flask.svg";
+import springImg from "./svg/spring.svg";
+import socketioImg from "./svg/socketio.svg";
+import unityImg from "./svg/unity.svg";
+import kotlinImg from "./svg/kotlin.svg";
+import githubImg from "./svg/github.svg";
 
 
-const ProjectBlock = ({title, dates, summary, weblink, gitlink}) => {
+
+const ProjectBlock = ({title, dates, summary, weblink, gitlink, skillsUsed}) => {
+
+    const skillBlocks = [
+        { img: javascriptImg, title: "Javascript" },
+        { img: cplusImg, title: "C++" },
+        { img: pythonImg, title: "Python" },
+        { img: javaImg, title: "Java" },
+        { img: htmlImg, title: "HTML" },
+        { img: cssImg, title: "CSS" },
+        { img: cImg, title: "C" },
+        { img: csharpImg, title: "C#" },
+        { img: kotlinImg, title: "Kotlin" },
+        { img: mongodbImg, title: "MongoDB" },
+        { img: mysqlImg, title: "MySQL" },
+        { img: nodejsImg, title: "Node.js" },
+        { img: reactImg, title: "React" },
+        { img: socketioImg, title: "Socket.io" },
+        { img: flaskImg, title: "Flask" },
+        { img: springImg, title: "Spring Boot" },
+        { img: unityImg, title: "Unity" },
+        { img: githubImg, title: "Github" },
+    ];
+    
     return (
         <>
             <div className="proj-container">
                 <h2 className="proj-title">{title} </h2>
                 <h3 className="proj-years">{dates}</h3>
+                <br />
+                <h3 className="skills-used">Skills Used: </h3> {skillsUsed && skillsUsed.map((skill) => {
+                    for(let skillBlock of skillBlocks) {
+                        if(skillBlock.title === skill) {
+                            return <img src={skillBlock.img} alt={skillBlock.title} className="proj-skill-img" />
+                        }
+                    }
+                })}
                 <p>{summary}</p>
                 <div className="links-container">
                     {weblink && <a className="link-link inner-btn" href={weblink} target="_blank" rel="noreferrer">
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -7,27 +7,32 @@ const Projects = () => {
             <h1 className="title" id="proj-section">Personal Projects:</h1>
 
             <ProjectBlock title={"Love Letter Game Simulator"} dates={"May 2022 - Present"} 
-                summary={"Created a card game simulator using C++, designed to be played by 2-4 players. The website is built with a ReactJS frontend using a custom Bootstrap theme and a Express.js with Socket.io backend, enabling multiplayer support. MongoDB is used to store game and user data."}
+                summary={"Created a card game simulator designed to be played by 2-4 players. The website is built with a ReactJS frontend using a custom Bootstrap theme and a Express.js with Socket.io backend, enabling multiplayer support. MongoDB is used to store game and user data."}
                 weblink={"https://loveletter.herokuapp.com"}
                 gitlink={"https://github.com/StevenAHan/Love-Letter-Emulator-Public"}
+                skillsUsed={["Javascript", "CSS", "React", "Express.js", "Socket.io", "Bootstrap","MongoDB"]}
                 projImg={loveletter}
             />
 
             <ProjectBlock title={"Invest.me"} dates={"January 2023 - May 2023"} 
                 summary={"I developed with a team of 4 people a customizable stock pricing tool, which uses both quantitative and qualitative data from numerous sources, including company financials, ESG scores, congressional trades, and public sentiment to evaluate S&P 500 companies. Our product utilizes web scraping, data analytics, and a linear regression ML model to predict stock prices Users can personalize results by inputting their own custom information and preferences."}
                 gitlink={"https://github.com/StevenAHan/Invest.me"}
+                skillsUsed={["Python", "Flask", "HTML", "CSS", "Javascript"]}
+
             />
 
             <ProjectBlock title={"Steven's Minigames"} dates={"November 2022 - May 2023"} 
                 summary={"Created a website that hosted solvers I created using Python Flask. The solvers were created with C++ to be more efficient. Frontend created with HTML/CSS/JS and Bootstrap. Currently, I have created solvers for the popular 24 Game, Sudoku, and the Discord Activity Spellcast"}
                 weblink={"https://stevens-minigames.herokuapp.com"}
                 gitlink={"https://github.com/StevenAHan/Stevens-Minigames"}
+                skillsUsed={["Python", "Flask", "HTML", "CSS", "Bootstrap", "C++", "Heroku"]}
             />
 
             <ProjectBlock title={"SoftSound"} dates={"June 2021 - June 2022"} 
                 summary={"Created a mobile application based on Kotlin that provides a stree-free space for people by playing calming sounds. Works offline, and enables users to customize the timing and volume of the sounds."}
                 // weblink={"https://stevens-minigames.herokuapp.com"}
                 gitlink={"https://github.com/StevenAHan/SoftSound"}
+                skillsUsed={["Kotlin", "Android Studio"]}
             />
         </>
     );
