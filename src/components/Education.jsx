b60582734a7cb14467b11c4df4c4ea1330713c68
majikyouser <sahiagb@gmail.com>
2023-07-02 10:56:39 -0400

    made code more readable

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -6,13 +6,15 @@ const Education = ({toggleNYU, toggleEBHS}) => {
             <h1 className="title" id="education-section">Education:</h1>
 
             <EducationBlock name={"New York University"} major={"Computer Science (BS)"} dates={"September 2021 - Present"} location={"New York, NY"}
-            summary={"Relevant Coursework: Operating Systems, Databases, Applied Internet Technologies, Computer Architecture, Object Oriented Programming, Data Structures and Algorithms, Artificial Intelligence, Discrete Math, Data Analysis"}
+            summary={`Relevant Coursework: Operating Systems, Databases, Applied Internet Technologies, Computer Architecture, 
+            Object Oriented Programming, Data Structures and Algorithms, Artificial Intelligence, Discrete Math, Data Analysis`}
             weblink={"https://www.nyu.edu/"} classes={"clickable"} toggleEd={toggleNYU}
             gpa={"3.945"}
             />
 
             <EducationBlock name={"East Brunswick High School"} dates={"September 2017 - June 2021"} location={"East Brunswick, NJ"}
-            summary={"Took 11 AP courses including computer science, math, physics, chemistry, language, history | SAT: 1550 (770 English, 780 Math); SAT II: 800 (Math II), 790 (Physics), 780 (Biology Ecological)"}
+            summary={`Took 11 AP courses including computer science, math, physics, chemistry, language, history 
+            | SAT: 1550 (770 English, 780 Math); SAT II: 800 (Math II), 790 (Physics), 780 (Biology Ecological)`}
             weblink={"https://www.ebnet.org/EBHS"}
             major={"High School Diploma"} classes={"clickable"}
             gpa={"Weighted: 4.610 | Unweighted: 3.941"} toggleEd={toggleEBHS}
--- src/components/WorkExperience.jsx
+++ src/components/WorkExperience.jsx
@@ -10,19 +10,26 @@ const WorkExperience = () => {
             <h1 className="title" id="work-section">Work Experience:</h1>
             
             <WorkBlock name={"Bank Of America"} title={"Software Engineer Intern"} dates={"June 2023 - Present"} location={"Pennington, NJ"}
-            summary={"I work primarily in Java Spring Boot to help create, test, and maintain the retail bill payment services for Bank of America. I work in a cooperative Agile team environment utilizing tools like Jira and Bitbucket to create documentation, test with J-unit testing, and work directly with application creation."}
+            summary={`I work primarily in Java Spring Boot to help create, test, and maintain the retail bill payment services for 
+            Bank of America. I work in a cooperative Agile team environment utilizing tools like Jira and Bitbucket to create 
+            documentation, test with J-unit testing, and work directly with application creation.`}
             weblink={"https://www.bankofamerica.com/"}
             logo={bofa}
             />
 
             <WorkBlock name={"Henkel"} title={"Research and Development Intern"} dates={"June 2022 - August 2022"} location={"Bridgewater, NJ"}
-            summary={"I investigated sustainable adhesive alternatives that are biodegradable and compostable with a leading global company, gaining research skills in a world-class industrial environment. I was in charge of designing, formulating, and testing products using scientific instruments and proprietary software. I also analyzed and processed data in Henkel's internal databases."}
+            summary={`I investigated sustainable adhesive alternatives that are biodegradable and compostable with a leading global 
+            company, gaining research skills in a world-class industrial environment. I was in charge of designing, formulating, and 
+            testing products using scientific instruments and proprietary software. I also analyzed and processed data in Henkel's 
+            internal databases.`}
             weblink={"https://www.henkel-northamerica.com/"}
             logo={henkel}
             />
 
             <WorkBlock name={"Augmented Libraries (VIP)"} title={"Backend Developer"} dates={"September 2021 - May 2022"} location={"New York, NY"}
-            summary={"As a backend developer for the NYU Augmented Libraries Vertically Integrated Project (VIP), I helped develop an intuitive and access-friendly IOS/Android-based AR application for NYU's Dibner Library with a team of 20+. I was a part of the backend subteam and established backend databasing infrastructure using MongoDB and SQLite."}
+            summary={`As a backend developer for the NYU Augmented Libraries Vertically Integrated Project (VIP), I helped develop 
+            an intuitive and access-friendly IOS/Android-based AR application for NYU's Dibner Library with a team of 20+. I was a 
+            part of the backend subteam and established backend databasing infrastructure using MongoDB and SQLite.`}
             weblink={"https://engineering.nyu.edu/research-innovation/student-research/vertically-integrated-projects/vip-teams/augmented-library"}
             logo={nyu}
             />
